77ce57760198e185005317e94b7e370f39a03ef9
Ravi2810 <a00226683@mycambrian.ca>
2021-04-19 22:20:51 -0400

    Fourth Commit

--- AnimalInfo/AnimalInfo/Page7.xaml.cs
+++ AnimalInfo/AnimalInfo/Page7.xaml.cs
@@ -16,5 +16,45 @@ namespace AnimalInfo
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_4(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_5(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_6(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_7(object sender, EventArgs e)
+        {
+
+        }
     }
 }
